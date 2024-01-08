c4126d249d23753602570bff7999c28dabe5e14a
Demolliderish <demolliderish@gmail.com>
2024-01-08 21:28:57 +1300

    main

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict"
+import demzoPlayer from ('./dist/player.js');
+import UtilsVideoOptions from ('./dist/utils/options.js');
+import FullScreenControls from ('./dist/utils/fullscreen.js');
+
+module.exports = {
+    demzoPlayer,
+    UtilsVideoOptions,
+    FullScreenControls
+};
